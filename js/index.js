b3cdb0d790cf814bfbbc2c2c134f7e79fd0a849d
abcgoods <605797173@qq.com>
2018-06-19 17:33:53 +0800

    限时福利，销量爆品，口碑好货

---
+++ js/index.js
@@ -0,0 +1,58 @@
+var Main1 = {
+    data() {
+        return {
+            CostInfo: [
+                {value: "free", label: '111'},
+                {value: "cost", label: '2222'},
+            ],
+            value: ''
+        };
+    }
+};
+var Ctor = Vue.extend(Main1);
+new Ctor().$mount('#myVue1');
+
+
+var Main2 = {
+    data() {
+        return {
+            CostInfo: [
+                {value: "free", label: '111'},
+                {value: "cost", label: '2222'},
+            ],
+            value: ''
+        };
+    }
+};
+var Ctor = Vue.extend(Main2);
+new Ctor().$mount('#myVue2');
+
+
+var Main3 = {
+    data() {
+        return {
+            CostInfo: [
+                {value: "free", label: '111'},
+                {value: "cost", label: '2222'},
+            ],
+            value: ''
+        };
+    }
+};
+var Ctor = Vue.extend(Main3);
+new Ctor().$mount('#myVue3');
+
+
+var Main4 = {
+    data() {
+        return {
+            CostInfo: [
+                {value: "free", label: '111'},
+                {value: "cost", label: '2222'},
+            ],
+            value: ''
+        };
+    }
+};
+var Ctor = Vue.extend(Main4);
+new Ctor().$mount('#myVue4');
